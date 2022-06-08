9db256a695047967aa8965b5baa35869047551ad
James Hercock <jamolo.hercock@hotmail.com>
2022-06-08 17:03:11 +0800

    Refactor to use github actions to trigger terraform

--- main.tf
+++ main.tf
@@ -13,20 +13,6 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "lambda-bucket"
-
-  tags = {
-    Name        = "Lambda Bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
-}
-
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
@@ -50,17 +36,17 @@ EOF
 resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a 
   # path.module in the filename.
-  filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
+  filename      = "artifacts/lambda_function_payload.zip"
+  function_name = "hello_world"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "index.test"
+  handler       = "hello.lambda_handler"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
   source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
-  runtime = "nodejs12.x"
+  runtime = "python3.9"
 
   environment {
     variables = {
