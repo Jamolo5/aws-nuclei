fb0c025ca2e23f966bbf8167cb872208de9e7f4c
James Hercock <jamolo.hercock@hotmail.com>
2022-06-10 15:44:28 +0800

    correct format for terraform

--- main.tf
+++ main.tf
@@ -68,63 +68,64 @@ resource "aws_lambda_function" "test_lambda" {
     }
   }
 
-  resource "aws_apigatewayv2_api" "lambda" {
-    name          = "serverless_lambda_gw"
-    protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = "serverless_lambda_gw"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  name        = "serverless_lambda_stage"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
   }
+}
 
-  resource "aws_apigatewayv2_stage" "lambda" {
-    api_id = aws_apigatewayv2_api.lambda.id
+resource "aws_apigatewayv2_integration" "test_lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
 
-    name        = "serverless_lambda_stage"
-    auto_deploy = true
+  integration_uri    = aws_lambda_function.test_lambda.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
 
-    access_log_settings {
-      destination_arn = aws_cloudwatch_log_group.api_gw.arn
+resource "aws_apigatewayv2_route" "test_lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
 
-      format = jsonencode({
-        requestId               = "$context.requestId"
-        sourceIp                = "$context.identity.sourceIp"
-        requestTime             = "$context.requestTime"
-        protocol                = "$context.protocol"
-        httpMethod              = "$context.httpMethod"
-        resourcePath            = "$context.resourcePath"
-        routeKey                = "$context.routeKey"
-        status                  = "$context.status"
-        responseLength          = "$context.responseLength"
-        integrationErrorMessage = "$context.integrationErrorMessage"
-        }
-      )
-    }
-  }
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.test_lambda.id}"
+}
 
-  resource "aws_apigatewayv2_integration" "test_lambda" {
-    api_id = aws_apigatewayv2_api.lambda.id
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
 
-    integration_uri    = aws_lambda_function.test_lambda.invoke_arn
-    integration_type   = "AWS_PROXY"
-    integration_method = "POST"
-  }
+  retention_in_days = 3
+}
 
-  resource "aws_apigatewayv2_route" "test_lambda" {
-    api_id = aws_apigatewayv2_api.lambda.id
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
 
-    route_key = "GET /hello"
-    target    = "integrations/${aws_apigatewayv2_integration.test_lambda.id}"
-  }
-
-  resource "aws_cloudwatch_log_group" "api_gw" {
-    name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
-
-    retention_in_days = 3
-  }
-
-  resource "aws_lambda_permission" "api_gw" {
-    statement_id  = "AllowExecutionFromAPIGateway"
-    action        = "lambda:InvokeFunction"
-    function_name = aws_lambda_function.test_lambda.function_name
-    principal     = "apigateway.amazonaws.com"
-
-    source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
-  }
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
