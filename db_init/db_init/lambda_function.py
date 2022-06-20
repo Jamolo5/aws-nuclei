c74920252acb62b1d41155837c36e1304b9719d2
James Hercock <jamolo.hercock@hotmail.com>
2022-06-20 14:01:34 +0800

    Add logging output to see if database init runs

--- db_init/db_init/lambda_function.py
+++ db_init/db_init/lambda_function.py
@@ -2,6 +2,7 @@ import boto3
 
 def lambda_handler(event, context):
     # TODO implement db table creation
+    print("Initialising Database")
     return {
         'statusCode': 200,
         'body': "Hello"
