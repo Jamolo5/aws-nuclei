73c452113f164a37fe23cc0b20c6fe8f677daa96
James Hercock <jamolo.hercock@hotmail.com>
2022-06-23 11:37:14 +0800

    Add secrets manager to manage db password, update db_init to use secret and give it permissions to access secret

--- db_init/db_init/lambda_function.py
+++ db_init/db_init/lambda_function.py
@@ -1,6 +1,8 @@
 import boto3
 import os
 import pymysql.cursors
+import base64
+from botocore.exceptions import ClientError
 
 connection = None
 def get_connection():
@@ -26,6 +28,56 @@ def get_connection():
     except Exception as e:
         print("While connecting failed due to :{0}".format(str(e)))
         return None
+
+# Sample code straight from AWS
+def get_secret():
+
+    secret_name = "arn:aws:secretsmanager:us-west-2:847035122536:secret:vuln_db_creds-scYaoD"
+    region_name = "us-west-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    # We rethrow the exception by default.
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS key.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            return get_secret_value_response['SecretString']
+        else:
+            return base64.b64decode(get_secret_value_response['SecretBinary'])
 
 def lambda_handler(event, context):
     print("Initialising Database")
