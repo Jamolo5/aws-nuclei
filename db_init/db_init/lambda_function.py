e247b72302eb38878b4203bfcf0a425e6a26aa3c
James Hercock <jamolo.hercock@hotmail.com>
2022-06-29 14:45:17 +0800

    Update scanner to push to db

--- db_init/db_init/lambda_function.py
+++ db_init/db_init/lambda_function.py
@@ -1,6 +1,6 @@
 import boto3
 import os
-import pymysql.cursors
+import pymysql
 import base64
 from botocore.exceptions import ClientError
 
@@ -8,11 +8,10 @@ connection = None
 def get_connection():
     try:
         print("Connecting to database")
-        client = boto3.client("rds")
         DBEndPoint = os.environ.get("DB_HOST")
         DBUserName = os.environ.get("DB_USER", "test")
         DBName = os.environ.get("APP_DB_NAME")
-        password = get_secret()
+        password = get_password()
         conn = pymysql.connect(
             host=DBEndPoint,
             user=DBUserName,
@@ -28,8 +27,8 @@ def get_connection():
         return None
 
 # Sample code straight from AWS
-def get_secret():
-    print("Fetching Secret")
+def get_password():
+    print("Fetching DB password")
     secret_name = os.environ.get("APP_DB_PW")
     region_name = os.environ.get("APP_REGION")
 
@@ -50,7 +49,7 @@ def get_secret():
             SecretId=secret_name
         )
     except ClientError as e:
-        print("Error:\n")
+        print("Error:")
         print(e)
         if e.response['Error']['Code'] == 'DecryptionFailureException':
             # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
@@ -82,21 +81,20 @@ def get_secret():
             return base64.b64decode(get_secret_value_response['SecretBinary'])
 
 def lambda_handler(event, context):
-    print("Initialising Database")
     global connection
     TableName = os.environ.get("APP_DB_NAME")
     try:
         if connection is None:
-            print("No existing connection, connecting..")
+            print("No existing DB connection, connecting..")
             connection = get_connection()
         if connection is None:
-            print("Connection could not be established, aborting")
+            print("Connection to DB could not be established, aborting")
             return {"status": "Error", "message": "Failed"}
         print("instantiating the cursor from connection")
         response = ""
         with connection:
             with connection.cursor() as cursor:
-                query = "CREATE TABLE {0} (id INT PRIMARY KEY AUTO_INCREMENT, timestamp_of_discovery TIMESTAMP, severity ENUM('info', 'low', 'medium', 'high', 'critical', 'unknown'), cve_or_name varchar(255), url varchar(255), additional_info varchar(255))".format(TableName)
+                query = "CREATE TABLE {0} (id INT PRIMARY KEY AUTO_INCREMENT, timestamp_of_discovery TIMESTAMP, severity ENUM('info', 'low', 'medium', 'high', 'critical', 'unknown'), cve_or_name varchar(255), category varchar(255), url varchar(255), additional_info varchar(255))".format(TableName)
                 print("Query:\n"+query)
                 cursor.execute(query)
                 results = cursor.fetchall()
@@ -108,7 +106,7 @@ def lambda_handler(event, context):
                 # retry = False
                 response = {"status": "Success", "results": str(results)}
             connection.commit()
-            return response
+        return response
     except Exception as e:
         print("Failed due to :{0}".format(str(e)))
         try:
